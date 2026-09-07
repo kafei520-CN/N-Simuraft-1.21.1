55e3ab92c4bd835ecd21d2e9e82dded34dc5998c
xiaoliang-china <2375505742@qq.com>
2026-09-07 21:58:40 +0800

    银行、交易所控制箱贴图更换。活跃的3个控制箱、彩虹灯块还原了老版本的多帧动画。

--- src/main/java/common/cn/kafei/simukraft/block/BankControlBoxBlock.java
+++ src/main/java/common/cn/kafei/simukraft/block/BankControlBoxBlock.java
@@ -3,15 +3,21 @@ package common.cn.kafei.simukraft.block;
 import common.cn.kafei.simukraft.bank.BankControlBoxService;
 import common.cn.kafei.simukraft.network.bank.BankControlBoxOpenRequestPacket;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.phys.BlockHitResult;
 
@@ -19,6 +25,29 @@ import net.minecraft.world.phys.BlockHitResult;
 public final class BankControlBoxBlock extends Block {
     public BankControlBoxBlock() {
         super(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).strength(1.0F).sound(SoundType.METAL));
+        registerDefaultState(stateDefinition.any().setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.NORTH));
+    }
+
+    /** getStateForPlacement: 放置时让控制箱正面朝向玩家。 */
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(
+                BlockStateProperties.HORIZONTAL_FACING,
+                context.getHorizontalDirection().getOpposite());
+    }
+
+    /** rotate: 结构旋转时同步水平朝向。 */
+    @Override
+    protected BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(
+                BlockStateProperties.HORIZONTAL_FACING,
+                rotation.rotate(state.getValue(BlockStateProperties.HORIZONTAL_FACING)));
+    }
+
+    /** mirror: 结构镜像时同步水平朝向。 */
+    @Override
+    protected BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(BlockStateProperties.HORIZONTAL_FACING)));
     }
 
     /** useWithoutItem: 空手右键打开银行控制箱。 */
@@ -37,5 +66,11 @@ public final class BankControlBoxBlock extends Block {
             BankControlBoxService.onRemoved(serverLevel, pos);
         }
         super.onRemove(state, level, pos, newState, movedByPiston);
+    }
+
+    /** createBlockStateDefinition: 注册控制箱的水平朝向属性。 */
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(BlockStateProperties.HORIZONTAL_FACING);
     }
 }
